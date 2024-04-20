124e6e1f7c21238085f5d82eb5dc668bf3e77f2e
Xingjian Zhang <jimmyzxj@umich.edu>
2024-04-20 19:26:27 -0400

    Add baseline scripts.

---
+++ baseline.py
@@ -0,0 +1,16 @@
+import argparse
+
+from recbole.quick_start import run_recbole
+
+parser = argparse.ArgumentParser()
+parser.add_argument("model", type=str, choices=["gru4rec", "sasrec"])
+parser.add_argument("dataset", type=str, choices=["ml-1m", "mind-small-train"])
+parser.add_argument("model_config", type=str)
+parser.add_argument("dataset_config", type=str)
+args = parser.parse_args()
+
+results = run_recbole(
+    model=args.model,
+    dataset=args.dataset,
+    config_file_list=[args.model_config, args.dataset_config],
+)
